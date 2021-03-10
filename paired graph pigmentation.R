49321fffefa9cbcc2057ae88430edc10869ca9e6
Radu Olariu <raduolariu@gmail.com>
2021-03-10 09:33:22 +0100

    2nd commit - changes to paired graph pigmentation

--- paired graph pigmentation.R
+++ paired graph pigmentation.R
@@ -1,4 +1,6 @@
 library(readr)
 cryo_test <- read_csv("cryo test.csv")
 library(ggpubr)
-ggpaired(cryo_test, "Pigmentation_preop", "Pigmentation_postop", color = "condition", fill= "condition", palette = "npg", width = 0.2)
+ggpaired(cryo_test, "Pigmentation_preop", "Pigmentation_postop", color = "condition", fill= "condition", palette = "jco", width = 0.2)
+
+
