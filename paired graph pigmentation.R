32bf866bdd97e5fb580595f1d4bd880abedffdb3
Radu Olariu <raduolariu@gmail.com>
2021-04-05 22:55:06 +0200

    observer scale calculations added

--- paired graph pigmentation.R
+++ paired graph pigmentation.R
@@ -1,4 +1,65 @@
 library(readr)
-cryo_test <- read_csv("cryo test.csv")
+cryo_observer <- read_csv("cryo_observer_scale.csv")
 library(ggpubr)
+boxplot(cryo_test$OBS_Thickness_preop, cryo_test$OBS_Thickness_postop,range = 0)
+shapiro.test(cryo_test$OBS_Thickness_preop)
+shapiro.test(cryo_test$Total_Observer_Score_preop)
+shapiro.test(rnorm(100, mean = 5, sd = 3))
+shapiro.test(runif(100, min = 2, max = 4))
+runif(100, min = 2, max = 4)
+itching_diff <- cryo_test$OBS_Thickness_preop - cryo_test$OBS_Thickness_postop
+mean(itching_diff)
+sd(itching_diff)
+obs_thickness_diff <- itching_diff
+rm(itching_diff)
+pliability_diff <- cryo_test$Pliability_preop - cryo_test$Pliability_postop
+mean(pliability_diff)
+sd(pliability_diff)
 
+
+library(dplyr)
+head(cryo_observer)
+
+#Thickness
+mean(filter(cryo_observer, cryo_observer$Time == "preop") %>% select(OBS_Thickness) %>% unlist)
+sd(filter(cryo_observer, cryo_observer$Time == "preop") %>% select(OBS_Thickness) %>% unlist)
+
+mean(filter(cryo_observer, cryo_observer$Time == "postop") %>% select(OBS_Thickness) %>% unlist)
+sd(filter(cryo_observer, cryo_observer$Time == "postop") %>% select(OBS_Thickness) %>% unlist)
+
+#Pliability
+mean(filter(cryo_observer, cryo_observer$Time == "preop") %>% select(Pliability) %>% unlist)
+sd(filter(cryo_observer, cryo_observer$Time == "preop") %>% select(Pliability) %>% unlist)
+
+mean(filter(cryo_observer, cryo_observer$Time == "postop") %>% select(Pliability) %>% unlist)
+sd(filter(cryo_observer, cryo_observer$Time == "postop") %>% select(Pliability) %>% unlist)
+
+t.test(filter(cryo_observer, cryo_observer$Time == "preop") %>% select(Pliability) %>% unlist,filter(cryo_observer, 
+cryo_observer$Time == "postop") %>% select(Pliability) %>% unlist, paired = TRUE)
+
+#Pigmentation
+mean(filter(cryo_observer, cryo_observer$Time == "preop") %>% select(Pigmentation) %>% unlist)
+sd(filter(cryo_observer, cryo_observer$Time == "preop") %>% select(Pigmentation) %>% unlist)
+
+mean(filter(cryo_observer, cryo_observer$Time == "postop") %>% select(Pigmentation) %>% unlist)
+sd(filter(cryo_observer, cryo_observer$Time == "postop") %>% select(Pigmentation) %>% unlist)
+
+t.test(filter(cryo_observer, cryo_observer$Time == "preop") %>% select(Pigmentation) %>% unlist,filter(cryo_observer, 
+ cryo_observer$Time == "postop") %>% select(Pigmentation) %>% unlist, paired = TRUE)
+
+#Relief
+mean(filter(cryo_observer, cryo_observer$Time == "preop") %>% select(Relief) %>% unlist)
+sd(filter(cryo_observer, cryo_observer$Time == "preop") %>% select(Relief) %>% unlist)
+
+mean(filter(cryo_observer, cryo_observer$Time == "postop") %>% select(Relief) %>% unlist)
+sd(filter(cryo_observer, cryo_observer$Time == "postop") %>% select(Relief) %>% unlist)
+
+t.test(filter(cryo_observer, cryo_observer$Time == "preop") %>% select(Relief) %>% unlist,filter(cryo_observer, 
+                                                                                                       cryo_observer$Time == "postop") %>% select(Relief) %>% unlist, paired = TRUE)
+
+
+mean(filter(cryo_observer, cryo_observer$Time == "postop") %>% select(Vascularization) %>% unlist)
+sd(filter(cryo_observer, cryo_observer$Time == "postop") %>% select(Vascularization) %>% unlist)
+
+t.test(filter(cryo_observer, cryo_observer$Time == "preop") %>% select(OBS_Thickness) %>% unlist,filter(cryo_observer, 
+cryo_observer$Time == "postop") %>% select(OBS_Thickness) %>% unlist, paired = TRUE)
